a85db7a96a36589abc5468d9d0672212e9ecb05e
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-23 16:24:49 +0300

    Upload File 2-O

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include <stddef.h>
+#include "sort.h"
+/**
+ * selection_sort - Implementation of selection_sort using C language
+ * @array: the array needed to sorted
+ * @size: the size of the array 
+ * Return: Didn't Return anything it sort array in place
+*/
+
+
+void selection_sort(int *array, size_t size)
+{
+	int arr_length = size;
+	int i = 0;
+	int j = 0;
+	int min,tmp;
+
+
+	for (i = 0; i < arr_length; i++)
+	{
+		min = i;
+		for (j = i; j < arr_length; j++)
+		{
+			if (array[min] > array[j])
+			{
+				min = j;
+			}
+		}
+		tmp = array[i];
+		array[i] = array[min];
+		array[min] = tmp;
+		
+		print_array(array, arr_length);
+	}
+}
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcc -Wall -Wextra -Werror -pedantic  -std=gnu89  $1 $2 print_list.c
+gcc -Wall -Wextra -Werror -pedantic  -std=gnu89  $1 $2 print_array.c
 ./a.out
